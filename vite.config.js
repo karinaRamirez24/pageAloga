d3a7292fe4690e7c2c688ecaa1a8f1e7d9c54225
karina ramirez <karina.ramirez.aloga@outlook.com>
2025-06-24 16:01:44 -0600

    Ul list content

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-        host: '0.0.0.0', // Permite acceso desde cualquier IP en la red local
-        port: 5175,      // Asegúrate de usar el puerto correcto
-      },
+ 
 })
